06da5db36179d470d45da71e008cb940cfa6c2bd
iiHaste <88557866+DasTech7@users.noreply.github.com>
2022-08-16 17:40:22 +0100

    Update Editor.jsx

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -2,16 +2,15 @@ import { useState } from "react";
 
 function Editor() {
   const [note, setNote] = useState(`yes
-jejer
-rhrhrhrhrhshhdbd
-dhdhdjdjdnr`);
+yarn is better package manager
+fly :)`);
   
   return (
     <textarea
       id="message"
       onChange={(el) => setNote(el.target.value)}
       value={note}
-      className="block font-mono p-2 outline-none h-[90vh] w-full text-sm text-gray-100 bg-gray-900 border-none"
+      className="block resize-none font-mono p-2 outline-none h-[90vh] w-full text-sm text-gray-100 bg-gray-900 border-none"
       placeholder="Your notes..."
     ></textarea>
   );
